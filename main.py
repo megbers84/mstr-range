9ce2c31bc1a890f0f1e1e8c5c328292ae0a54674
megbers84 <144559069+megbers84@users.noreply.github.com>
2025-08-02 14:59:20 -0500

    Update main.py

--- main.py
+++ main.py
@@ -97,5 +97,10 @@ def index():
 
     return render_template("index.html", result=result, defaults=default_data)
 
+
+
+import os
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=81)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
